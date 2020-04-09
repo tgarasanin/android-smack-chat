fc38efc73cdeacfa5ce733309e1afdfd21cbd5b9
Teodora Garasanin <t.garasanin@levi9.com>
2020-04-09 15:41:02 +0200

    add date formatter

--- app/src/main/java/com/tgarasanin/smack/Adapters/MessageAdapter.kt
+++ app/src/main/java/com/tgarasanin/smack/Adapters/MessageAdapter.kt
@@ -1,14 +1,21 @@
 package com.tgarasanin.smack.Adapters
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SimpleItemAnimator
 import com.tgarasanin.smack.Model.Message
 import com.tgarasanin.smack.R
 import com.tgarasanin.smack.Service.UserDataService
 import kotlinx.android.synthetic.main.message_list_view.view.*
+import java.net.SocketImpl
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MessageAdapter(val context: Context, val messages: ArrayList<Message>): RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
 
@@ -20,7 +27,7 @@ class MessageAdapter(val context: Context, val messages: ArrayList<Message>): Re
             itemView.messageProfileImage.setBackgroundColor(UserDataService.returnAvatarColor(message.userAvatarColor))
             itemView.messageBodyTextView.text = message.message
             itemView.messageNameTextView.text = message.userName
-            itemView.messageTimestampTextView.text = message.timestamp
+            itemView.messageTimestampTextView.text = returnDataString(message.timestamp)
         }
     }
 
@@ -35,6 +42,20 @@ class MessageAdapter(val context: Context, val messages: ArrayList<Message>): Re
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder?.bindMessage(context, messages[position])
+    }
+
+    fun returnDataString(isoString: String) : String {
+        val isoFormatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
+        isoFormatter.timeZone = TimeZone.getTimeZone("UTC")
+        var convertedDate = Date()
+        try {
+            convertedDate = isoFormatter.parse(isoString)
+        } catch (e: ParseException) {
+            Log.d("PARSE", "Cannot parse date")
+        }
+
+        val outDateString = SimpleDateFormat("E, h:mm a", Locale.getDefault())
+        return outDateString.format(convertedDate)
     }
 
 }
